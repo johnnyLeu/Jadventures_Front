81b28363bda4cb938b0bb74a45ba1eb72ef81dcf
simone <simonemuziohaze@gmail.com>
2024-02-23 11:56:21 +0100

    v2

--- jadventures/src/App.js
+++ jadventures/src/App.js
@@ -1,27 +1,29 @@
+// App.js
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import Homepage from "./components/homepage/Homepage";
 import Navbar from "./components/navbar/Navbar";
 import PatronLogin from "./components/login/PatronLogin";
 
-
-//Variabile di contesto globale
-//è STATE dell'intera applicazione;
-
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />    
+      <Navbar isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="patronlogin" element={<PatronLogin />} />
-
-        {/*
-        <Route path="allpeople" element={<AllPeople />} />
-        <Route path="persondetail/:id" element={<PersonDetail />}/>
-         in spring facciamo con url/{id} 
-        */}
-        </Routes>
+        <Route path="patronlogin" element={<PatronLogin handleLogin={handleLogin} isAuthenticated={isAuthenticated} />} />
+      </Routes>
     </BrowserRouter>
   );
 }
